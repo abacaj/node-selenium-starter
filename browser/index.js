7282d4b6937d157ae67c8dd2b632a2713b0f729a
Anton Bacaj <abacaj1@gmail.com>
2016-06-10 16:26:14 -0400

    Update index.js
    fix missing comma

--- browser/index.js
+++ browser/index.js
@@ -8,7 +8,7 @@ function getLocalBrowser() {
     		"Linux": "bin/linux/chromedriver",
     		"Darwin": "bin/mac/chromedriver",
     		"Windows_NT": "bin/windows/chromedriver.exe"
-    	}
+    	},
         exeLocation = path.resolve(__dirname, path.relative(__dirname, exeName[os.type()])),
         service = new chrome.ServiceBuilder(exeLocation).build(),
         driver = new chrome.Driver(webdriver.Capabilities.chrome(), service),
